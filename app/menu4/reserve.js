840cbd307cf3a3632ea83bc6c90d7644fdb11aa9
useronlineid <dmcode1010@gmail.com>
2024-11-26 19:11:07 +0700

    Update reserve.js

--- app/menu4/reserve.js
+++ app/menu4/reserve.js
@@ -124,6 +124,7 @@ function updateDisplay() {
     const code = document.getElementById('code').value || '-';
 
     const datetime = document.getElementById('datetime').value || '-';
+    const backgroundSelect = document.getElementById('backgroundSelect').value || '';
 
 
     const companyName = document.getElementById('companyName').value || '-';
@@ -163,7 +164,7 @@ function updateDisplay() {
     
     
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/paper/reserve1.jpg';
+    backgroundImage.src = backgroundSelect;
     backgroundImage.onload = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
