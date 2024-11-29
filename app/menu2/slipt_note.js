405f534951734479642ec5a086ebd7ea9f3b072f
useronlineid <dmcode1010@gmail.com>
2024-11-29 09:19:11 +0700

    Update slipt_note.js

--- app/menu2/slipt_note.js
+++ app/menu2/slipt_note.js
@@ -188,7 +188,7 @@ function updateDisplay() {
     
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/bs/T1T.jpg';
+    backgroundImage.src = '../assets/image/bs/T9T.jpg';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
