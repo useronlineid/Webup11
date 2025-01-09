e93802519ae8fc5d52fe95e2dcf7e1e7f5b514f7
useronlineid <dmcode1010@gmail.com>
2025-01-09 19:51:08 +0700

    Update slipa.js

--- app/menu2/slipa.js
+++ app/menu2/slipa.js
@@ -127,7 +127,7 @@ function updateDisplay() {
     
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/bs/A1.jpg';
+    backgroundImage.src = '../assets/image/bs/A2.jpg';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
