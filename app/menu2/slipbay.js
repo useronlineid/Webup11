907e63149d3899d4aa8f02866fb46ab533cd9486
useronlineid <dmcode1010@gmail.com>
2025-03-13 23:15:09 +0700

    Update slipbay.js

--- app/menu2/slipbay.js
+++ app/menu2/slipbay.js
@@ -186,6 +186,9 @@ function updateDisplay() {
         case 'ICBC':
             bankLogoUrl = '../assets/image/logo/ICBC.png';
             break;
+        case 'PromptPay':
+            bankLogoUrl = '../assets/image/logo/P-Krungsri.png';
+            break;
     }
 
     const formattedDate = formatDate(datetime);
