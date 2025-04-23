768f22913ba6631ae175a6a2c6fbd8a6671ca9c2
useronlineid <dmcode1010@gmail.com>
2025-04-23 14:13:15 +0700

    Update E-slipb_note.js

--- app/menu2/E-slipb_note.js
+++ app/menu2/E-slipb_note.js
@@ -151,7 +151,7 @@ function updateDisplay() {
     
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/bs/B8T.jpg';
+    backgroundImage.src = '../assets/image/bs/B9T.jpg';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
