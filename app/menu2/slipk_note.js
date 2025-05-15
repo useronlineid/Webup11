f33ffd491bc987c2ebeb9c2969d6904cf660e6db
useronlineid <dmcode1010@gmail.com>
2025-05-15 16:31:08 +0700

    Update slipk_note.js

--- app/menu2/slipk_note.js
+++ app/menu2/slipk_note.js
@@ -238,10 +238,10 @@ function updateDisplay() {
             break;
         case 'รหัสพร้อมเพย์':
             bankText = 'รหัสพร้อมเพย์';
-            bankLogoUrl = '../assets/image/logo/KBANK-P.png';
+            bankLogoUrl = '../assets/image/logo/P-KBANK.png';
             break;
         case 'พร้อมเพย์วอลเล็ท':
-            bankLogoUrl = '../assets/image/logo/KBANK-P.png';
+            bankLogoUrl = '../assets/image/logo/P-KBANK.png';
             break;
         case 'MetaAds':
             bankLogoUrl = '../assets/image/logo/Meta.png';
