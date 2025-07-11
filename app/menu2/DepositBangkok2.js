49767359cee301d97d9c3b7086880ba533822488
useronlineid <dmcode1010@gmail.com>
2025-07-11 12:31:00 +0700

    Update DepositBangkok2.js

--- app/menu2/DepositBangkok2.js
+++ app/menu2/DepositBangkok2.js
@@ -143,7 +143,7 @@ function updateDisplay() {
 
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/bs/backgroundEnter-B2.jpg';
+    backgroundImage.src = '../assets/image/bs/backgroundEnter-B2.1.jpg';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
