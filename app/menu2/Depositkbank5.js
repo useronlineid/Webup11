a70815fe335f4fcbbf3e837c467bdc90083c04f5
useronlineid <dmcode1010@gmail.com>
2025-09-29 20:40:25 +0700

    Update Depositkbank5.js

--- app/menu2/Depositkbank5.js
+++ app/menu2/Depositkbank5.js
@@ -177,7 +177,7 @@ function updateDisplay() {
 
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/bs/backgroundEnter-KB5.jpg';
+    backgroundImage.src = '../assets/image/bs/backgroundEnter-KB5.1.jpg';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
