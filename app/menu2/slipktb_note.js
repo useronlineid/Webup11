4b1d48b5f08e2f41b14f0f9421dded1ac55db2dc
useronlineid <dmcode1010@gmail.com>
2025-10-25 10:24:56 +0700

    Update slipktb_note.js

--- app/menu2/slipktb_note.js
+++ app/menu2/slipktb_note.js
@@ -210,7 +210,7 @@ function updateDisplay() {
     
     // Load background image
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/bs/KTB15T.jpg';
+    backgroundImage.src = '../assets/image/bs/KTB16T.jpg';
     backgroundImage.onload = function() {
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
