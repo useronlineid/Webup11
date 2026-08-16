eea1e993f0534c25ac312ca32054b8db9a32c619
useronlineid <dmcode1010@gmail.com>
2026-08-16 12:40:52 +0700

    Update slipktb.js

--- app/menu2/slipktb.js
+++ app/menu2/slipktb.js
@@ -217,6 +217,10 @@ function updateDisplay() {
             bankText = 'MetaAds2';
             bankLogoUrl = '../assets/image/logo/Meta4.png'; 
             break;
+        case 'CLICX':
+            bankText = 'CLICX';
+            bankLogoUrl = '../assets/image/logo/CLICX1.png'; 
+            break;
     }
 
 
